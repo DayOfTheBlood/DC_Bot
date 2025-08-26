0c65ea20920654a2bf913a9c973972e8246a7a68
DayOfTheBlood <dayoftheblood@gmail.com>
2025-08-26 16:28:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2229,9 +2229,9 @@ class TeamSwapConfirmView(discord.ui.View):
         await self._finalize(
             interaction,
             f"{self.target.mention} moved from {self.from_role.mention} to {self.to_role.mention} (by {self.requester.mention}).{extra}"
-        )
+            )
 
-    )
+        )
 
 
     @discord.ui.button(label="Decline", style=discord.ButtonStyle.danger)
